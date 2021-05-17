202b2a6f0e0b76b36d65ed99b132c12330ce828b
jocampo <jocampo@EN2010407.endava.net>
2021-05-17 15:37:43 -0500

    Configure pom.xml file to execute Main class

---
+++ WordFinder/src/main/java/control/Main.java
@@ -0,0 +1,23 @@
+package control;
+
+import java.util.Collection;
+import java.util.Scanner;
+import model.WordFinder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Type a string to find all the English words it contains:");
+		Scanner sc = new Scanner(System.in);
+		String word = sc.nextLine();
+		sc.close();
+		
+		WordFinder wordFinder = new WordFinder();
+		Collection<String> words = wordFinder.findAllEnglishWords(word);
+
+		for (String englishWord : words) {
+			System.out.println(englishWord);
+		}
+	}
+	
+}
