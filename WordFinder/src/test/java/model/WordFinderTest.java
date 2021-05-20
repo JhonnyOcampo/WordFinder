73c22f84c9c34b35327199f96146e6cf54c4993c
jocampo <jocampo@EN2010407.endava.net>
2021-05-19 19:36:51 -0500

    Implement AssertJ for assertions

--- WordFinder/src/test/java/model/WordFinderTest.java
+++ WordFinder/src/test/java/model/WordFinderTest.java
@@ -1,112 +1,84 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Collection;
+import java.util.Set;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 class WordFinderTest {
 
 	private WordFinder wordFinder = new WordFinder();
-	
 
 	@Test
-	void testFindAllEnglishWords() {
-		
-		Collection<String> result1 = wordFinder.findAllEnglishWords("WORKING");
-		assertTrue(result1.contains("working"));
-		assertTrue(result1.contains("work"));
-		assertTrue(result1.contains("king"));
-		assertTrue(result1.contains("row"));
-		assertTrue(result1.contains("ring"));
-		assertTrue(result1.contains("know"));	
-		assertTrue(result1.contains("wrong"));
-		assertTrue(result1.contains("or"));
-		assertTrue(result1.contains("go"));
-		assertTrue(result1.contains("ok"));
-		assertTrue(result1.contains("in"));		
-		assertFalse(result1.contains("wrkng"));
-		assertFalse(result1.contains("rk"));		
-		
-		Collection<String> result2 = wordFinder.findAllEnglishWords("working");
-		assertTrue(result2.contains("working"));
-		assertTrue(result2.contains("work"));
-		assertTrue(result2.contains("king"));
-		assertTrue(result2.contains("row"));
-		assertTrue(result2.contains("ring"));
-		assertTrue(result2.contains("know"));	
-		assertTrue(result2.contains("wrong"));
-		assertTrue(result2.contains("or"));
-		assertTrue(result2.contains("go"));
-		assertTrue(result2.contains("ok"));
-		assertTrue(result2.contains("in"));		
-		assertFalse(result2.contains("wrkng"));
-		assertFalse(result2.contains("rk"));	
-				
-		Collection<String> result3 = wordFinder.findAllEnglishWords("eye");	
-		int counter = 0;
-		for (String r3 : result3) {
-			if(r3.equals("eye")) {
-				counter++;
-			}
-		}
-		assertTrue(counter == 1);
-				
-		Collection<String> result4 = wordFinder.findAllEnglishWords("qwrtpsdfgh");	
-		assertTrue(result4.isEmpty());	
-		
-		Collection<String> result5 = wordFinder.findAllEnglishWords(null);	
-		assertTrue(result5.isEmpty());	
-				
-		Collection<String> result6 = wordFinder.findAllEnglishWords("!#$%&/()=?¡");	
-		assertTrue(result6.isEmpty());	
-		
-		Collection<String> result7 = wordFinder.findAllEnglishWords("!W#O$R%K&I/N()G=?¡");	
-		assertTrue(result7.contains("working"));
-		assertTrue(result7.contains("work"));
-		assertTrue(result7.contains("king"));
-		assertTrue(result7.contains("row"));
-		assertTrue(result7.contains("ring"));
-		assertTrue(result7.contains("know"));	
-		assertTrue(result7.contains("wrong"));
-		assertTrue(result7.contains("or"));
-		assertTrue(result7.contains("go"));
-		assertTrue(result7.contains("ok"));
-		assertTrue(result7.contains("in"));		
-		assertFalse(result7.contains("wrkng"));
-		assertFalse(result7.contains("rk"));
-		
-		Collection<String> result8 = wordFinder.findAllEnglishWords("excellent");	
-		assertTrue(result8.contains("next"));
-		assertTrue(result8.contains("cent"));
-		assertTrue(result8.contains("tell"));
-		assertTrue(result8.contains("let"));
-		assertTrue(result8.contains("net"));
-		assertTrue(result8.contains("teen"));
-		assertTrue(result8.contains("excel"));
-		assertTrue(result8.contains("excellent"));
-		assertFalse(result8.contains("xllt"));
-		assertFalse(result8.contains("le"));
-		
-		Collection<String> result9 = wordFinder.findAllEnglishWords("an");	
-		assertTrue(result9.contains("an"));
-	
+	void nullEntry() {
+		assertThat(wordFinder.findAllEnglishWords(null)).hasSize(0);
 	}
-	
-	
+
 	@Test
-	void testPerfomanceFindAllEnglishWords() {
-		Collection<String> result10 = wordFinder.findAllEnglishWords("hippopotamus");	
-		assertTrue(result10.contains("ship"));
-		assertTrue(result10.contains("path"));
-		assertTrue(result10.contains("mouth"));
-		assertTrue(result10.contains("shampoo"));
-		assertTrue(result10.contains("hippopotamus"));
-		assertTrue(result10.contains("spam"));
-		assertTrue(result10.contains("autism"));
-		assertTrue(result10.contains("oath"));
-		assertTrue(result10.contains("atom"));
+	void doesNotContainEnglishWords() {
+		assertThat(wordFinder.findAllEnglishWords("qwrtpsdfgh")).hasSize(0);
+	}
+
+	@Test
+	void doesNotContainDuplicates() {
+		assertThat(wordFinder.findAllEnglishWords("eye")).containsOnlyOnce("eye");
+	}
+
+	@Test
+	void inputWithSpecialCharacters() {
+		SoftAssertions softly = new SoftAssertions();
+		softly.assertThat(wordFinder.findAllEnglishWords("!#$%&/()=?¡")).hasSize(0);
+		softly.assertThat(wordFinder.findAllEnglishWords("W#o$R%k*In(G"))
+				.contains("working", "work", "king", "row", "ring", "know", "wrong", "or", "go", "ok", "in")
+				.doesNotContain("wrkng", "rk");
+		softly.assertAll();
+	}
+
+	@Test
+	void stringWithSpaces() {
+		assertThat(wordFinder.findAllEnglishWords("a nice day"))
+				.contains("a", "nice", "day", "ace", "dice", "ice", "dance", "icy", "an", "aid", "idea", "can", "any")
+				.doesNotHaveDuplicates();
+	}
+
+	@Test
+	void oneLetterWord() {
+		assertThat(wordFinder.findAllEnglishWords("a")).contains("a");
+	}
+
+	@Test
+	void twoLetterWord() {
+		assertThat(wordFinder.findAllEnglishWords("an")).contains("a", "an");
+	}
+
+	@Test
+	void sevenLetterWord() {
+		Collection<String> expectedWords = Set.of("working", "work", "king", "row", "ring", "know", "wrong", "or", "go",
+				"ok", "in");
+		Collection<String> unexpectedWords = Set.of("wrkng", "rk");
+		SoftAssertions softly = new SoftAssertions();
+		softly.assertThat(wordFinder.findAllEnglishWords("WORKING")).containsAll(expectedWords)
+				.doesNotContainAnyElementsOf(unexpectedWords);
+		softly.assertThat(wordFinder.findAllEnglishWords("working")).containsAll(expectedWords)
+				.doesNotContainAnyElementsOf(unexpectedWords);
+		softly.assertAll();
+	}
+
+	@Test
+	void nineLetterWord() {
+		assertThat(wordFinder.findAllEnglishWords("excellent"))
+				.contains("next", "cent", "tell", "let", "net", "teen", "excel", "excellent")
+				.doesNotContain("xllt", "le").doesNotHaveDuplicates();
+	}
+
+	@Test
+	void twelveLetterWord() {
+		assertThat(wordFinder.findAllEnglishWords("hippopotamus"))
+				.contains("ship", "path", "mouth", "shampoo", "hippopotamus", "spam", "autism", "oath", "atom")
+				.doesNotHaveDuplicates();
 	}
 
 }
